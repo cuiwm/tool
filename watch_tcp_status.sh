e11a2f45d3f0c8d77dd6702d883b737ad5adaa9e
cuiwm <xcuiwm@hotmail.com>
2017-05-02 18:13:10 +0800

    watch tcp connection's status

---
+++ watch_tcp_status.sh
@@ -0,0 +1,9 @@
+#########################################################################
+# File Name: watch_tcp_status.sh
+# Author: andrew Choe
+# mail: xgod@163.com
+# Created Time: Tue 02 May 2017 03:46:16 PM CST
+#########################################################################
+#!/bin/bash
+
+watch -n 1 -d './netstat_tcp_status.sh'
